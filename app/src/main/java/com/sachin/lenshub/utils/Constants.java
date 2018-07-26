5a8cb2482c34b5ae247a29ec8eab9800fad08dd1
dev.sachin-kushwah <sachin.kushwah>
2018-07-26 18:44:22 +0530

    Test again

--- app/src/main/java/com/sachin/lenshub/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.sachin.lenshub.utils;
-
-// Created by sachin singh on 20/7/18.
-
-public class Constants {
-    public static final int VIEW_TYPE_SECTION = 1;
-    public static final int VIEW_TYPE_NORMAL = 0;
-}
--- app/src/main/java/com/sachin/lenshub/utils/Utils.java
+++
@@ -1,6 +0,0 @@
-package com.sachin.lenshub.utils;
-
-// Created by sachin singh on 20/7/18.
-
-public class Utils {
-}
